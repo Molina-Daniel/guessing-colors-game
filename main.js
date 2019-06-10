ecdef71e111966933f91fc95c7fcf88ba854783f
Daniel Molina <dmolcap@gmail.com>
2019-06-10 18:31:45 +0200

    game logic working

--- main.js
+++ main.js
@@ -1,39 +1,69 @@
-let colors = [];
-let pickedColor = null;
+let colors = generateRandomColors(6);
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay")
 let squares = document.querySelectorAll(".square");
+let resultDisplay = document.getElementById("result");
 
-init();
 console.log(colors);
 
-function init() {
-  pickRandomColors();
-  displayColors();
-  pickedColor = colors[Math.floor(Math.random() * 7)];
-  colorDisplay.innerHTML = pickedColor;
+colorDisplay.innerHTML = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+  const square = squares[i];
+
+  square.style.backgroundColor = colors[i];
+
+  square.addEventListener("click", function () {
+    let clickedColor = this.style.backgroundColor
+    if (clickedColor == pickedColor) {
+      console.log("Correct!!");
+      resultDisplay.innerHTML = "Correct!!"
+      changeColors(pickedColor)
+    } else {
+      console.log("Fail!!");
+      console.log(pickedColor);
+      this.style.backgroundColor = "#fff";
+      resultDisplay.innerHTML = "Try Again!!"
+    }
+  })
+
 }
 
-function pickRandomColors() {
-  for (let i = 0; i < squares.length; i++) {
-    let r = Math.floor(Math.random() * 256)
-    let g = Math.floor(Math.random() * 256)
-    let b = Math.floor(Math.random() * 256)
-    let randomColor = "rgb(" + r + ", " + g + ", " + b + ")"
-    colors.push(randomColor);
 
-    squares[i].addEventListener("click", function () {
-      let clickedColor = this.style.backgroundColor
-      if (clickedColor == pickedColor) {
-        console.log("Correct!!");
-      } else {
-        console.log("Fail!!");
-      }
-    })
+function generateRandomColors(num) {
+  let colorsArr = [];
+
+  for (let i = 0; i < num; i++) {
+    colorsArr.push(randomColor());
   }
+
+  return colorsArr;
+}
+
+function randomColor() {
+  let r = Math.floor(Math.random() * 256)
+  let g = Math.floor(Math.random() * 256)
+  let b = Math.floor(Math.random() * 256)
+  let randomColor = "rgb(" + r + ", " + g + ", " + b + ")"
+
+  return randomColor;
 }
 
 function displayColors() {
   for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
+  }
+}
+
+function pickColor() {
+  let random = Math.floor(Math.random() * colors.length);
+
+  return colors[random]
+}
+
+function changeColors(color) {
+  for (let i = 0; i < squares.length; i++) {
+    const square = squares[i];
+    square.style.backgroundColor = color;
   }
 }
