01d3fa28f1bc6b7a299f12445d2d596a9ebb229b
Daniel Molina <dmolcap@gmail.com>
2019-06-10 19:01:59 +0200

    added reset button

--- main.js
+++ main.js
@@ -3,6 +3,23 @@ let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay")
 let squares = document.querySelectorAll(".square");
 let resultDisplay = document.getElementById("result");
+let resetButton = document.getElementById("reset");
+let header = document.getElementById("header");
+
+resetButton.addEventListener("click", function () {
+  //generate all new colors
+  colors = generateRandomColors(6);
+  //pick a new random color from array
+  pickedColor = pickColor();
+  //change colorDisplay to match picked Color
+  colorDisplay.innerHTML = pickedColor;
+  //change colors of squares
+  for (let i = 0; i < squares.length; i++) {
+    const square = squares[i];
+    square.style.backgroundColor = colors[i];
+  }
+  header.style.backgroundColor = "#232323";
+})
 
 console.log(colors);
 
@@ -17,18 +34,19 @@ for (let i = 0; i < squares.length; i++) {
     let clickedColor = this.style.backgroundColor
     if (clickedColor == pickedColor) {
       console.log("Correct!!");
-      resultDisplay.innerHTML = "Correct!!"
-      changeColors(pickedColor)
+      resultDisplay.innerHTML = "Correct!!";
+      resetButton.innerHTML = "Play Again?";
+      changeColors(pickedColor);
+      header.style.backgroundColor = pickedColor;
     } else {
       console.log("Fail!!");
       console.log(pickedColor);
-      this.style.backgroundColor = "#fff";
+      this.style.backgroundColor = "#232323";
       resultDisplay.innerHTML = "Try Again!!"
     }
   })
 
 }
-
 
 function generateRandomColors(num) {
   let colorsArr = [];
